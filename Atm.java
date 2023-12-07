0ac1d4940c439c4d040faaa1f4ab7ee3122fac7a
PayalDahe <103224589+PayalDahe@users.noreply.github.com>
2023-12-07 16:14:16 +0530

    Update Atm.java

--- Atm.java
+++ Atm.java
@@ -1,16 +1,13 @@
    
 import java.util.Scanner;  
-  
-//create ATMExample class to implement the ATM functionality  
-public class ATMExample  
+public class ATM
 {  
-    //main method starts   
+   
     public static void main(String args[] )  
-    {  
-        //declare and initialize balance, withdraw, and deposit  
-        int balance = 100000, withdraw, deposit;  
+    {   
+        int balance = 100000;
+        int withdraw, deposit;  
           
-        //create scanner class object to get choice of user  
         Scanner sc = new Scanner(System.in);  
           
         while(true)  
@@ -22,53 +19,43 @@ public class ATMExample
             System.out.println("Choose 4 for EXIT");  
             System.out.print("Choose the operation you want to perform:");  
               
-            //get choice from user  
             int choice = sc.nextInt();  
             switch(choice)  
             {  
                 case 1:  
-        System.out.print("Enter money to be withdrawn:");  
-                      
-        //get the withdrawl money from user  
-        withdraw = sc.nextInt();  
-                      
-        //check whether the balance is greater than or equal to the withdrawal amount  
-        if(balance >= withdraw)  
-        {  
-            //remove the withdrawl amount from the total balance  
-            balance = balance - withdraw;  
-            System.out.println("Please collect your money");  
-        }  
-        else  
-        {  
-            //show custom error message   
-            System.out.println("Insufficient Balance");  
-        }  
-        System.out.println("");  
-        break;  
+                   System.out.print("Enter money to be withdrawn:");  
+                   withdraw = sc.nextInt(); 
+                   if(balance >= withdraw)  
+                    {  
+                      balance = balance - withdraw;  
+                      System.out.println("Please collect your money");  
+                    }  
+                  else  
+                   {     
+                    System.out.println("Insufficient Balance");  
+                    }  
+                    System.out.println("");  
+                   break;  
    
                 case 2:  
                       
-        System.out.print("Enter money to be deposited:");  
+                System.out.print("Enter money to be deposited:");  
                       
-        //get deposite amount from te user  
-        deposit = sc.nextInt();  
-                      
-        //add the deposit amount to the total balanace  
-        balance = balance + deposit;  
-        System.out.println("Your Money has been successfully depsited");  
-        System.out.println("");  
-        break;  
+               deposit = sc.nextInt();  
+               
+               balance = balance + deposit;  
+               System.out.println("Your Money has been successfully depsited");  
+              System.out.println("");  
+              break;  
    
                 case 3:  
-        //displaying the total balance of the user  
-        System.out.println("Balance : "+balance);  
-        System.out.println("");  
-        break;  
+
+                 System.out.println("Balance : "+balance);  
+                 System.out.println("");  
+               break;  
    
-                case 4:  
-        //exit from the menu  
-        System.exit(0);  
+                case 4:    
+            System.exit(0);  
             }  
         }  
     }  
